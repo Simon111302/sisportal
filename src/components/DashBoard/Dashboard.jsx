d73f36ed4095297013ee3f4dff75905d87896434
Simon111302 <probelenia9@gmail.com>
2026-01-18 01:09:22 +0800

    scureded

--- src/components/DashBoard/Dashboard.jsx
+++ src/components/DashBoard/Dashboard.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import '../../design/Dashboard.css';
-import API_URL from '../../server/config/api';
+import { API_URL } from '../../config/api'
 
 function Dashboard() {
   const [user, setUser] = useState(null);
